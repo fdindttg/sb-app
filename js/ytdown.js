03b6fba9ba71475ad850a2098c9f377f5c0bdde1
fdindttg@gmail.com <fdindttg@gmail.com>
2023-08-18 13:59:46 +0800

    1

--- js/ytdown.js
+++ js/ytdown.js
@@ -1,9 +1,9 @@
 var cm_app_link = {
-	"an_link" : "https://i84w2.ccgyszr.com/095tx",
-	"an_yk_link": "https://i84w2.ccgyszr.com/095tx",
-	"cj_link" : "https://i84w2.ccgyszr.com/095tx",
-	"cj_link2" : "https://i84w2.ccgyszr.com/095tx",
-	"server_link" : "https://i84w2.ccgyszr.com/095tx",
-	"pao_ios_zc_link" : "https://i84w2.ccgyszr.com/095tx",
-	"pao_ios_link" : "https://i84w2.ccgyszr.com/095tx"
+	"an_link" : "https://i84w2.ixthdvx.com/095tx",
+	"an_yk_link": "https://i84w2.ixthdvx.com/095tx",
+	"cj_link" : "https://i84w2.ixthdvx.com/095tx",
+	"cj_link2" : "https://i84w2.ixthdvx.com/095tx",
+	"server_link" : "https://i84w2.ixthdvx.com/095tx",
+	"pao_ios_zc_link" : "https://i84w2.ixthdvx.com/095tx",
+	"pao_ios_link" : "https://i84w2.ixthdvx.com/095tx"
 }
